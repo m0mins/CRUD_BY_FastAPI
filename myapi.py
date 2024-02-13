0eb5d97b93324fad301ceeaaedfa74992e7c1555
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-13 17:19:59 +0600

    get by name DONE

--- myapi.py
+++ myapi.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI,Path
 app=FastAPI()
 
 students= {
-    1: {"name": "John Doe", "dept": "Computer Science", "student_id": "CS12345"},
+    1: {"name": "John", "dept": "Computer Science", "student_id": "CS12345"},
     2: {"name": "Jane Smith", "dept": "Electrical Engineering", "student_id": "EE67890"},
     3: {"name": "Bob Johnson", "dept": "Mechanical Engineering", "student_id": "ME54321"},
     4: {"name": "Alice Brown", "dept": "Physics", "student_id": "PH98765"},
@@ -19,11 +19,20 @@ students= {
 @app.get("/")
 def index():
     return {"name":"Momin"}
-
+#List data
 @app.get("/all/students")
 def all_students():
     return students
-
+#retrive by Id
 @app.get("/get-student/{student_id}")
-def get_student(student_id:int= Path(...,description="The ID of the student you want to view.")):
+def get_student(student_id:int= Path(...,description="The ID of the student you want to view.",gt=0,lt=10)):
+    #gt=Greater than, lt=less than, ge=Greater than equal ,le=less than equal
     return students[student_id]
+
+@app.get("/get-by-name")
+def get_student(name:str):
+    for student_id in students:
+        if students[student_id]["name"]==name:
+            return students[student_id]
+        return {"Message":"Data not found"}
+    
